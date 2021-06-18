e6e2cc45fc93f3fe2ad3e38e70ba22aca44c2d07
Dikiy Maulana <Dikymaulana732@gmail.com>
2021-06-19 00:58:24 +0700

    minggu 11

---
+++ Minggu 11/diky_project/app/Http/Controllers/backend/PendidikanController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+
+use Illuminate\Http\Request,
+    App\Http\Controllers\Controller;
+use App\Models\Pendidikan;
+
+class PendidikanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pendidikan = Pendidikan::get();
+        return view('backend.pendidikan.index',compact('pendidikan'));
+    }
+
+    /**
+
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $pendidikan = null;
+        return view('backend.pendidikan.create',compact('pendidikan'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Pendidikan::create($request->all());
+        
+        return redirect()->route('pendidikan.index')
+                        ->with('success','Data Pendidikan baru telah berhasil disimpan.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pendidikan $pendidikan)
+    {
+        return view('backend.pendidikan.create', compact('pendidikan'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pendidikan $pendidikan)
+    {
+        $pendidikan->update($request->all());
+
+        return redirect()->route('pendidikan.index')
+                        ->with('succes','Pendidikan berhasil diperbarui.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pendidikan $pendidikan)
+    {
+        $pendidikan->delete();
+        return redirect()->route('pendidikan.index')
+                        ->with('successs', 'Data Pendidikan berhasil dihapus');
+    }
+}
